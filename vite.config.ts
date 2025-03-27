222dd2ba010276a93d5d850a18daf6ea28072088
ertembiyik <ertembiyik@gmail.com>
2025-03-27 23:22:07 +0300

    modify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 3000,
-  },
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    optimizeDeps: {
+      exclude: ["lucide-react"],
+    },
+    server: {
+      port: 3000,
+      host: true, // Listen on all IPv4 and IPv6 addresses
+      allowedHosts: [process.env.APP_DOMAIN],
+    },
+    preview: {
+      port: 3000,
+      host: true, // Listen on all IPv4 and IPv6 addresses
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+  });
+};
