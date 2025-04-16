6dacdd28959cafc63fe9b58f55d881e4dd1dfe1d
ertembiyik <ertembiyik@gmail.com>
2025-04-16 17:32:33 +0300

    fix App.tsx generation

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
+// !--IMPORTANT: DO NOT DELETE THE FOLLOWING code or this comment, only add/replace/delete routes--!
+
 function App() {
   useEffect(() => {
     Bluerage.init()
