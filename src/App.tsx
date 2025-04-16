6b451777a99b72135a77925792a92f6c4dd7a593
ertembiyik <ertembiyik@gmail.com>
2025-04-16 17:32:54 +0300

    move App.tsx comment up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+// !--IMPORTANT: DO NOT DELETE THE FOLLOWING code or this comment, only add/replace/delete routes--!
+
 import { useEffect } from 'react'
 import { SafeAreaLayout } from './components/SafeAreaLayout'
 import { Bluerage } from './Bluerage'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
-// !--IMPORTANT: DO NOT DELETE THE FOLLOWING code or this comment, only add/replace/delete routes--!
 
 function App() {
   useEffect(() => {
